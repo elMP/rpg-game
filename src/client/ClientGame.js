25d7a3ca61082cd3e5650a1796a2d9cda9aceef1
kneazle <kneazle@yandex.ru>
2021-07-16 01:50:13 +0300

    move player

--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -3,16 +3,23 @@ import ClientWorld from './ClientWorld';
 
 import sprites from '../configs/sprites';
 import levelCfg from '../configs/world.json';
+import gameObjects from '../configs/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
     Object.assign(this, {
       cfg,
+      gameObjects,
+      player: null,
     });
 
     this.engine = this.createEngine();
     this.map = this.createWorld();
     this.initEngine();
+  }
+
+  setPlayer(player) {
+    this.player = player;
   }
 
   createEngine() {
@@ -25,11 +32,45 @@ class ClientGame {
 
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
-      console.log('ENGINE', this.engine);
+      this.map.init();
       this.engine.on('render', (_, time) => {
-        this.map.init();
+        this.map.render(time);
       });
       this.engine.start();
+      this.initKeys();
+    });
+  }
+
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (keydown) => {
+        if (keydown) {
+          this.playerMoves(-1, 0);
+        }
+      },
+      ArrowRight: (keydown) => {
+        if (keydown) {
+          this.playerMoves(1, 0);
+        }
+      },
+      ArrowUp: (keydown) => {
+        if (keydown) {
+          this.playerMoves(0, -1);
+        }
+      },
+      ArrowDown: (keydown) => {
+        if (keydown) {
+          this.playerMoves(0, 1);
+        }
+      },
+    });
+  }
+
+  playerMoves(x, y) {
+    this.player.moveByCellCoord(x, y, (cell) => {
+      console.log(cell.x, cell.y);
+      console.log(this.map.height, this.map.width);
+      return cell.findObjectsByType('grass').length;
     });
   }
 
